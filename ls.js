e2ecdc208da3e8eab6ad4f2b22e42ea780a561ec
Fredrik Bergljung <fredrik.bergljung@valtech.se>
2013-12-23 10:09:56 +0100

    removed unnecessary checks

--- ls.js
+++ ls.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var regex = new RegExp('\\.' + process.argv[3]);
 var buf = fs.readdir(process.argv[2], function (err, list) {
   for (var i = 0; i < list.length; i++) {
-    if (list[i] && 0 !== list[i].length && regex.test(list[i]))
+    if (regex.test(list[i]))
       console.log(list[i]);
   }
 });
