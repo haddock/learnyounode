bd7e7d79bb688966e447ac00d54086211eb1bafe
Fredrik Bergljung <fredrik.bergljung@valtech.se>
2014-01-03 09:37:03 +0100

    Solution for Http collect

---
+++ http_collect.js
@@ -0,0 +1,13 @@
+var bl = require('bl');
+var http = require('http');
+var url = process.argv[2];
+
+http.get(url, function(response) {
+  response.pipe(bl(function (err, data) {
+    if (err)
+      return console.error(data);
+    data = data.toString();
+    console.log(data.length);
+    console.log(data);
+  }));
+});
